5770cbc4f7e44d3c631a70526d77f15d7068bd2a
Allison Blumenthal <blumen.allison@gmail.com>
2023-01-11 09:50:28 -0500

    working on api call conversion

--- api/authorData.js
+++ api/authorData.js
@@ -1,9 +1,8 @@
-import axios from 'axios';
 import { clientCredentials } from '../utils/client';
 
 const dbUrl = clientCredentials.databaseURL;
 
-// FIXME:  GET ALL AUTHORS
+// DONE:  GET ALL AUTHORS
 const getAuthors = (uid) => new Promise((resolve, reject) => {
   fetch(`${dbUrl}/authors.json?orderBy="uid"&equalTo="${uid}"`, {
     method: 'GET',
@@ -21,9 +20,9 @@ const getAuthors = (uid) => new Promise((resolve, reject) => {
     .catch((error) => reject(error));
 });
 
-// FIXME: CREATE AUTHOR
+// DONE: CREATE AUTHOR
 const createAuthor = (payload) => new Promise((resolve, reject) => {
-  fetch(`${endpoint}/authors.json`, {
+  fetch(`${dbUrl}/authors.json`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -47,14 +46,45 @@ const getSingleAuthor = (firebaseKey) => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
-// FIXME: DELETE AUTHOR
-const deleteSingleAuthor = () => {};
+// DONE: DELETE AUTHOR
+const deleteSingleAuthor = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/authors/${firebaseKey}.json`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
 
-// FIXME: UPDATE AUTHOR
-const updateAuthor = () => {};
+// DONE: UPDATE AUTHOR
+const updateAuthor = (payload) => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/authors/${payload.firebaseKey}.json`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
 
-// TODO: GET A SINGLE AUTHOR'S BOOKS
-const getAuthorBooks = () => {};
+// DONE: GET A SINGLE AUTHOR'S BOOKS
+const getAuthorBooks = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/books.json?orderBy="author_id"&equalTo="${firebaseKey}"`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
 
 export {
   getAuthors,
