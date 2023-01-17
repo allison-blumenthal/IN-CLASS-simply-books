ec928d1a244d3ba3ecba791341fee69d6d82f6c9
Allison Blumenthal <blumen.allison@gmail.com>
2023-01-16 22:41:07 -0500

    working on getting author books

--- pages/author/[firebaseKey].js
+++ pages/author/[firebaseKey].js
@@ -15,18 +15,25 @@ useEffect(() => {
 }, [firebaseKey])
 
 return (
+  <>
   <div className="mt-5 d-flex flex-wrap">
     <div className="d-flex flex-column">
       <img src={author.image} alt={author.last_name} style={{ width: '300px' }} />
     </div>
     <div className="text-white ms-5 details">
       <h2>
-        {author.first_name} {author.last_name} 
+        {author.first_name} {author.last_name}
       </h2>
       <h6>{author.favorite ? <span class="badge text-bg-warning">Favorite</span> : ''}
       </h6>
       Author Email: <a href={`mailto:${author.email}`}>{author.email}</a>
     </div>
   </div>
+  <div>
+
+
+    
+  </div>
+  </>
   );
 }
--- pages/book/[firebaseKey].js
+++ pages/book/[firebaseKey].js
@@ -37,4 +37,3 @@ export default function ViewBook() {
     </div>
   );
 }
-c
