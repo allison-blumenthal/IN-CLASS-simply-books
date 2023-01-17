7db250f06954c65730347a62383c6c8146712b61
Allison Blumenthal <blumen.allison@gmail.com>
2023-01-16 22:27:27 -0500

    got view author working

--- pages/author/[firebaseKey].js
+++ pages/author/[firebaseKey].js
@@ -11,10 +11,22 @@ const router = useRouter();
 const { firebaseKey } = router.query;
 
 useEffect(() => {
-  viewAuthorDetails(firebaseKey).then()
-}, [])
+  viewAuthorDetails(firebaseKey).then(setAuthor);
+}, [firebaseKey])
 
 return (
-    <div>View Author</div>
+  <div className="mt-5 d-flex flex-wrap">
+    <div className="d-flex flex-column">
+      <img src={author.image} alt={author.last_name} style={{ width: '300px' }} />
+    </div>
+    <div className="text-white ms-5 details">
+      <h2>
+        {author.first_name} {author.last_name} 
+      </h2>
+      <h6>{author.favorite ? <span class="badge text-bg-warning">Favorite</span> : ''}
+      </h6>
+      Author Email: <a href={`mailto:${author.email}`}>{author.email}</a>
+    </div>
+  </div>
   );
 }
--- pages/book/[firebaseKey].js
+++ pages/book/[firebaseKey].js
@@ -37,3 +37,4 @@ export default function ViewBook() {
     </div>
   );
 }
+c
